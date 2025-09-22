d37888b7994d5a5cd8a30c9f25e084c0af7868c1
Anastasia Shilova <bunnnyyy@mail.ru>
2025-09-22 22:34:32 +0300

    res

--- sprint1/problems/restaurant_single_threaded/solution/src/main.cpp
+++ sprint1/problems/restaurant_single_threaded/solution/src/main.cpp
@@ -90,23 +90,6 @@ private:
 // Функция, которая будет вызвана по окончании обработки заказа
 using OrderHandler = std::function<void(sys::error_code ec, int id, Hamburger* hamburger)>;
 
-class Restaurant {
-public:
-    explicit Restaurant(net::io_context& io)
-        : io_(io) {
-    }
-
-    int MakeHamburger(bool with_onion, OrderHandler handler) {
-        const int order_id = ++next_order_id_;
-        std::make_shared<Order>(io_, order_id, with_onion, std::move(handler))->Execute();
-        return order_id;
-    }
-
-private:
-    net::io_context& io_;
-    int next_order_id_ = 0;
-};
-
 class Order : public std::enable_shared_from_this<Order> {
 public:
     Order(net::io_context& io, int id, bool with_onion, OrderHandler handler)
@@ -224,6 +207,23 @@ private:
     bool onion_marinaded_ = false;
     bool delivered_ = false;
 }; 
+
+class Restaurant {
+public:
+    explicit Restaurant(net::io_context& io)
+        : io_(io) {
+    }
+
+    int MakeHamburger(bool with_onion, OrderHandler handler) {
+        const int order_id = ++next_order_id_;
+        std::make_shared<Order>(io_, order_id, with_onion, std::move(handler))->Execute();
+        return order_id;
+    }
+
+private:
+    net::io_context& io_;
+    int next_order_id_ = 0;
+};
 
 int main() {
     net::io_context io;
