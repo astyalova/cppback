ece836587191900ea3907d7eac1b87ce47c6df14
Anastasia Shilova <bunnnyyy@mail.ru>
2025-09-24 13:06:46 +0300

    cafeteria+

--- sprint1/problems/cafeteria/solution/src/cafeteria.h
+++ sprint1/problems/cafeteria/solution/src/cafeteria.h
@@ -17,10 +17,9 @@
 
 namespace net = boost::asio;
 
-// Колбэк, вызываемый по завершении заказа
 using HotDogHandler = std::function<void(Result<HotDog>)>;
 
-class Cafeteria : public std::enable_shared_from_this<Cafeteria> {
+class Cafeteria {
 public:
     explicit Cafeteria(net::io_context& io)
         : io_(io)
@@ -28,20 +27,10 @@ public:
         , gas_cooker_(std::make_shared<GasCooker>(io_))
     {}
 
-    // Заказывает приготовление хот-дога.
-    // Можно вызывать из нескольких потоков одновременно.
     void OrderHotDog(HotDogHandler handler) {
-        // сохраняем shared_ptr, чтобы объект Cafeteria жил до конца заказа
-        auto self = shared_from_this();
-        net::dispatch(
-            strand_,
-            [self, handler = std::move(handler)]() mutable {
-                try {
-                    self->StartCooking(std::move(handler));
-                } catch (...) {
-                    handler(Result<HotDog>{std::current_exception()});
-                }
-            });
+        net::dispatch(strand_, [this, handler = std::move(handler)]() mutable {
+            StartCooking(std::move(handler));
+        });
     }
 
 private:
@@ -50,7 +39,6 @@ private:
     std::shared_ptr<GasCooker> gas_cooker_;
     Store store_;
 
-    // усреднённые времена готовки — гарантированно в допустимых интервалах
     static Clock::duration SausageTargetDuration() {
         return (HotDog::MIN_SAUSAGE_COOK_DURATION + HotDog::MAX_SAUSAGE_COOK_DURATION) / 2;
     }
@@ -60,55 +48,37 @@ private:
 
     void StartCooking(HotDogHandler handler) {
         auto sausage = store_.GetSausage();
-        auto bread   = store_.GetBread();
-        const int id = store_.NextOrderId();
+        auto bread = store_.GetBread();
+        int id = store_.NextOrderId();
 
-        // Жарим сосиску
-        sausage->StartFry(*gas_cooker_,
-            [self = shared_from_this(), sausage, bread, id, handler = std::move(handler)]() mutable {
-                auto timer = std::make_shared<net::steady_timer>(self->io_, SausageTargetDuration());
-                timer->async_wait(
-                    net::bind_executor(
-                        self->strand_,
-                        [self, sausage, bread, id, handler, timer](const boost::system::error_code& ec) mutable {
-                            if (ec) {
-                                handler(Result<HotDog>{std::make_exception_ptr(std::runtime_error(ec.message()))});
-                                return;
-                            }
-                            try {
-                                sausage->StopFry();
-                            } catch (...) {
-                                handler(Result<HotDog>{std::current_exception()});
-                                return;
-                            }
-                            self->BakeBread(std::move(sausage), std::move(bread), id, std::move(handler));
-                        }));
-            });
-    }
+        auto sausage_timer = std::make_shared<net::steady_timer>(io_, SausageTargetDuration());
+        auto bread_timer = std::make_shared<net::steady_timer>(io_, BreadTargetDuration());
 
-    void BakeBread(std::shared_ptr<Sausage> sausage,
-                   std::shared_ptr<Bread> bread,
-                   int id,
-                   HotDogHandler handler) {
-        bread->StartBake(*gas_cooker_,
-            [self = shared_from_this(), sausage, bread, id, handler = std::move(handler)]() mutable {
-                auto timer = std::make_shared<net::steady_timer>(self->io_, BreadTargetDuration());
-                timer->async_wait(
-                    net::bind_executor(
-                        self->strand_,
-                        [self, sausage, bread, id, handler, timer](const boost::system::error_code& ec) mutable {
-                            if (ec) {
-                                handler(Result<HotDog>{std::make_exception_ptr(std::runtime_error(ec.message()))});
-                                return;
-                            }
-                            try {
-                                bread->StopBaking();
-                                HotDog hotdog(id, sausage, bread);
-                                handler(Result<HotDog>{std::move(hotdog)});
-                            } catch (...) {
-                                handler(Result<HotDog>{std::current_exception()});
-                            }
-                        }));
-            });
+        sausage->StartFry(*gas_cooker_, [sausage_timer, this, sausage, bread, id, handler = std::move(handler), bread_timer]() mutable {
+            sausage_timer->async_wait(net::bind_executor(strand_, [this, sausage, bread, id, handler = std::move(handler), bread_timer](const boost::system::error_code& ec) mutable {
+                if (ec) {
+                    handler(Result<HotDog>{std::make_exception_ptr(std::runtime_error(ec.message()))});
+                    return;
+                }
+
+                try { sausage->StopFry(); }
+                catch (...) { handler(Result<HotDog>{std::current_exception()}); return; }
+
+                bread->StartBake(*gas_cooker_, [bread_timer, this, sausage, bread, id, handler = std::move(handler)]() mutable {
+                    bread_timer->async_wait(net::bind_executor(strand_, [sausage, bread, id, handler = std::move(handler)](const boost::system::error_code& ec) mutable {
+                        if (ec) {
+                            handler(Result<HotDog>{std::make_exception_ptr(std::runtime_error(ec.message()))});
+                            return;
+                        }
+
+                        try { bread->StopBaking(); }
+                        catch (...) { handler(Result<HotDog>{std::current_exception()}); return; }
+
+                        try { handler(Result<HotDog>{HotDog(id, sausage, bread)}); }
+                        catch (...) { handler(Result<HotDog>{std::current_exception()}); }
+                    }));
+                });
+            }));
+        });
     }
 };
