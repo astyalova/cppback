73c55d915f81cb05fdce76c75c499cb885ef497a
Anastasia Shilova <bunnnyyy@mail.ru>
2025-09-25 09:45:42 +0300

    map_json

--- sprint1/problems/map_json/solution/src/main.cpp
+++ sprint1/problems/map_json/solution/src/main.cpp
@@ -46,11 +46,11 @@ int main(int argc, const char* argv[]) {
         http_handler::RequestHandler handler{game};
 
         // 5. Запустить обработчик HTTP-запросов, делегируя их обработчику запросов
-        /*
+        
         http_server::ServeHttp(ioc, {address, port}, [&handler](auto&& req, auto&& send) {
             handler(std::forward<decltype(req)>(req), std::forward<decltype(send)>(send));
         });
-        */
+        
 
         // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
         std::cout << "Server has started..."sv << std::endl;
